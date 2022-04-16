ccf22346610a1f2235d8be35b81599d8dbf49f6c
bokob <bmc0520@naver.com>
2022-04-16 11:20:26 +0900

    함수 출력안되는 오류 수정

--- tools/print_type_matching_info.py
+++ tools/print_type_matching_info.py
@@ -7,7 +7,13 @@ data=pd.read_excel(path_file)   # 엑셀 파일 읽기
 
 df_list=data.values.tolist()    # 리스트화
 
-def print_type_matching_info(type): # 해당 분야에 맞는 과목 출력
+def print_type_matching_info(subject_type):     # 분야에 맞는 강의의 모든 정보 출력
+    print_list=[]
     for i in range(len(df_list)-1):
-        if type in df_list[i]:
-           print(df_list[i])
+        if subject_type in df_list[i]:
+           print_list.append( df_list[i] )
+    print(*print_list)
+
+x=input()
+
+print_type_matching_info(x)
