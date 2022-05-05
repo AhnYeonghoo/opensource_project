fa53bbcfeb7c37d94a019a2dd3da60e1ad881516
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-05 11:20:09 +0900

    Database migrate

--- evolve/search/admin.py
+++ evolve/search/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+admin.site.register(models.FirstSemesterMajor)
+admin.site.register(models.FirstBasicCulture)
+admin.site.register(models.FirstGaesinBasicCulture)
+admin.site.register(models.FirstSemesterSWMajor)
+admin.site.register(models.FirstNaturalScience)
+admin.site.register(models.SecondBasicCulture)
+admin.site.register(models.SecondGaesinBasicCulture)
+admin.site.register(models.SecondSemesterMajor)
+admin.site.register(models.SecondNaturalScience)
+admin.site.register(models.SecondSemesterSWMajor)
+admin.site.register(models.Ocu)
+
+
+
--- evolve/search/models.py
+++ evolve/search/models.py
@@ -1,3 +1,300 @@
 from django.db import models
+import pandas as pd
 
 # Create your models here.
+
+
+file = pd.read_csv("../etc/1학기_전공.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+# pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
+
+
+class FirstSemesterMajor(models.Model):
+    
+    # 1학기 전공에 대한 정보
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+
+# pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
+
+
+class SecondSemesterMajor(models.Model):
+    
+    # 2학기 전공
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+file = pd.read_csv("../etc/1학기_소웨전공.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+class FirstSemesterSWMajor(models.Model):
+    
+    # 1학기 소웨 전공
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+file = pd.read_csv("../etc/2학기_소웨전공.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+class SecondSemesterSWMajor(models.Model):
+ 
+    # 2학기 소웨 전공
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+
+
+file = pd.read_csv("../etc/1학기_자연이공계기초.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+class FirstNaturalScience(models.Model):
+    
+    # 1학기 자연이공계기초
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    def get_info(self):
+        return pd_file.values.tolist()
+
+
+file = pd.read_csv("../etc/2학기_자연이공계기초.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+
+class SecondNaturalScience(models.Model):
+    
+    # 2학기 자연이공계기초
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+
+file = pd.read_csv("../etc/1학기_ocu.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+
+class Ocu(models.Model):
+    
+    # 1학기,2학기 OCU
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+
+file = pd.read_csv("../etc/1학기_개신기초교양.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+
+class FirstGaesinBasicCulture(models.Model):
+    
+    # 1학기 개신기초교양
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+
+
+file = pd.read_csv("../etc/2학기_개신기초교양.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+class SecondGaesinBasicCulture(models.Model):
+    
+    # 2학기 개신기초교양
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+
+# file = pd.read_csv("../etc/1학기_일반교양.csv", encoding='utf-8' ,dtype=str)
+# file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+# pd_file = pd.DataFrame(file).dropna()
+
+
+
+class FirstBasicCulture(models.Model):
+    
+    # 1학기 일반교양
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
+    
+
+
+file = pd.read_csv("../etc/2학기_일반교양.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+
+
+
+class SecondBasicCulture(models.Model):
+    
+    # 2학기 일반교양
+    
+    학년 = models.CharField("학년", max_length=150)
+    # 학년 = pd_file['학년'].to_string()
+    과목구분 = models.CharField("과목구분", max_length=150)
+    # 과목구분 = pd_file['과목구분']
+    과목코드 = models.CharField("과목코드", max_length=150)
+    # 과목코드 = pd_file['과목코드']
+    과목명 = models.CharField("과목명", max_length=200)
+    # 과목명 = pd_file['과목명']
+    학점 = models.CharField("학점", max_length=150)
+    # 학점 = pd_file['학점']
+    학점부여 = models.CharField("학점부여", max_length=150)
+    # 학점부여 = pd_file['학점부여']
+    
+    def get_info(self):
+        return pd_file.values.tolist()
