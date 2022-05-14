0e5fa9646f94152e0a7b2294ac2031abba8e9d39
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-14 11:10:45 +0900

    키워드에 따른 추천과목리스트 출력 페이징

--- evolve/search/urls.py
+++ evolve/search/urls.py
@@ -7,4 +7,5 @@ app_name = "search"
 urlpatterns = [
     path("index/", views.index, name="index"),
     path("upload-file/", views.upload_file, name="upload_file"),
+    path('recomand/', views.recomand, name="recomand"),
 ]
--- evolve/search/views.py
+++ evolve/search/views.py
@@ -8,11 +8,18 @@ from openpyxl import load_workbook
 # Create your views here.
 
 def index(req):
-    gaesin = models.Lecture.objects.all()
-    context = {"gaesin" : gaesin }
-    return render(req, "index.html", context)
+    
+    '''
+    main page render를 위한 함수
+    '''
+    
+    return render(req, "index.html")
 
 def upload_file(req):
+    
+    '''
+    사용자에게 파일 업로드 받아서 원하는 title대로 DB에 저장해주는 함수
+    '''
     
     if req.method == "POST":
         title = req.POST.get('title')
@@ -45,3 +52,35 @@ def upload_file(req):
         
         
 
+def recomand(req):
+    
+    '''
+    게임, 앱, 네트워크, 임베디드, 인공지능 키워드에 따라 과목 출력해주는 함수
+    '''
+    
+    if req.POST.get("game"):
+        game_list = models.Game.objects.all()
+        context = {"game" : game_list}
+        return render(req, 'game_list.html', context)
+    
+    elif req.POST.get('ai'):
+        ai_list = models.Ai.objects.all()
+        context = {"ai": ai_list}
+        return render(req, "ai_list.html", context)
+    
+    elif req.POST.get('embedded'):
+        embed_list = models.Embedded.objects.all()
+        context = {"embedded": embed_list}
+        return render(req, "embed_list.html", context)
+    
+    elif req.POST.get('network'):
+        network_list = models.Network.objects.all()
+        context = {"network": network_list}
+        return render(req, "network_list.html", context)
+    
+    elif req.POST.get('app'):
+        app_list = models.Application.objects.all()
+        context = {"app": app_list}
+        return render(req, "app_list.html", context)
+    
+    
