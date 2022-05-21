4b1a8e010fb4d509afa8ba8232758bc14a24f1a9
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-21 14:08:30 +0900

    새로 추가된 api 라우팅

--- evolve/search/urls.py
+++ evolve/search/urls.py
@@ -14,5 +14,10 @@ urlpatterns = [
     path("read_user_lecture", views.read_user_lecture, name="read_user_lecture"),
     path("calculator", views.calculator, name="calculator"),
     path("get_my_lecture", views.get_my_lecture, name="get_my_lecture"),
+    path("get_new_lecture", views.Old_and_new.get_new_lecture, name="get_new_lecture"),
+    path("get_old_lecture", views.Old_and_new.get_old_lecture, name="get_old_lecture"),
+    path("get_all_lecture", views.Old_and_new.get_all_lecture, name="get_all_lecture"),
+    path("get_revision_lecture", views.Old_and_new.get_revision_lecture, name="get_revision_lecture"),
+    
     
 ]
