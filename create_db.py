7be51e5958f5676f5b9a93c493577ff687fefae6
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-21 10:36:11 +0900

    전공 파일 불러오기 및 학과 구분하여 변수에 저장

--- create_db.py
+++ create_db.py
@@ -1,10 +1,10 @@
-# import pandas as pd
+import pandas as pd
 
 
-# file = pd.read_csv("./교육과정/1학기_전공.csv", encoding='utf-8' ,dtype=str)
-# file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
-# pd_file = pd.DataFrame(file).dropna()
-# # pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
+file = pd.read_csv("./교육과정/1학기_전공.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
 
 # # 1학기 전공 DB
 # class FirstSemesterMajor:
--- temp.py
+++
@@ -1,8 +0,0 @@
-class Lecture:
-    
-    #생성자
-    # def __init__(self, 강의명, 이수구분 etc..):
-    
-    # getter 셋팅
-    # def get(self, )
-        
