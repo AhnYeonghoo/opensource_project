aae7b55fc620daa2a5fef3f7ebc0653d249ed3e1
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-21 10:45:02 +0900

    1학기 소웨 전공 데이터베이스 테스트

--- create_db.py
+++ create_db.py
@@ -48,18 +48,18 @@ file = file.filter(['학년','과목구분', '과목코드', '과목명', '학�
 pd_file = pd.DataFrame(file).dropna()
 # pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
 
-# # 1학기 소웨 전공
-# class FirstSemesterSWMajor:
+# 1학기 소웨 전공
+class FirstSemesterSWMajor:
     
-#     학년 = pd_file['학년']
-#     과목구분 = pd_file['과목구분']
-#     과목코드 = pd_file['과목코드']
-#     과목명 = pd_file['과목명']
-#     학점 = pd_file['학점']
-#     학점부여 = pd_file['학점부여']
+    학년 = pd_file['학년']
+    과목구분 = pd_file['과목구분']
+    과목코드 = pd_file['과목코드']
+    과목명 = pd_file['과목명']
+    학점 = pd_file['학점']
+    학점부여 = pd_file['학점부여']
     
-#     def get_info(self):
-#         return pd_file.values.tolist()
+    def get_info(self):
+        return pd_file.values.tolist()
     
 # fssm = FirstSemesterSWMajor()
 # fssm.get_info()
