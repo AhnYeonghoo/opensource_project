b0937c99d511fbf9fbadad12bb7d92e3bdcc1fe5
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-21 10:50:26 +0900

    2학기 자연이공계기초 데이터베이스 테스트

--- create_db.py
+++ create_db.py
@@ -113,18 +113,18 @@ file = file.filter(['학년','과목구분', '과목코드', '과목명', '학�
 pd_file = pd.DataFrame(file).dropna()
 # pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
 
-# # 2학기 자연이공계기초
-# class SecondNaturalScience:
+# 2학기 자연이공계기초
+class SecondNaturalScience:
     
-#     학년 = pd_file['학년']
-#     과목구분 = pd_file['과목구분']
-#     과목코드 = pd_file['과목코드']
-#     과목명 = pd_file['과목명']
-#     학점 = pd_file['학점']
-#     학점부여 = pd_file['학점부여']
+    학년 = pd_file['학년']
+    과목구분 = pd_file['과목구분']
+    과목코드 = pd_file['과목코드']
+    과목명 = pd_file['과목명']
+    학점 = pd_file['학점']
+    학점부여 = pd_file['학점부여']
     
-#     def get_info(self):
-#         return pd_file.values.tolist()
+    def get_info(self):
+        return pd_file.values.tolist()
     
 # sns = SecondNaturalScience()
 # sns.get_info()
