de61b13fcecb7bec1f850b6a51b73cb839486dcc
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-21 10:42:04 +0900

    1학기 전공 데이터베이스 테스트

--- create_db.py
+++ create_db.py
@@ -6,18 +6,18 @@ file = file.filter(['학년','과목구분', '과목코드', '과목명', '학�
 pd_file = pd.DataFrame(file).dropna()
 pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
 
-# # 1학기 전공 DB
-# class FirstSemesterMajor:
+# 1학기 전공 DB
+class FirstSemesterMajor:
     
-#     학년 = pd_file['학년']
-#     과목구분 = pd_file['과목구분']
-#     과목코드 = pd_file['과목코드']
-#     과목명 = pd_file['과목명']
-#     학점 = pd_file['학점']
-#     학점부여 = pd_file['학점부여']
+    학년 = pd_file['학년']
+    과목구분 = pd_file['과목구분']
+    과목코드 = pd_file['과목코드']
+    과목명 = pd_file['과목명']
+    학점 = pd_file['학점']
+    학점부여 = pd_file['학점부여']
     
-#     def get_info(self):
-#         return pd_file.values.tolist()
+    def get_info(self):
+        return pd_file.values.tolist()
     
 # fsm = FirstSemesterMajor()
 # fsm.get_info()
