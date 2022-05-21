1f1916ebbd8410edffe6115eba80da0dcc6cd7ea
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-21 10:54:16 +0900

    1학기 일반교양 파일 불러오기 및 결측치 제거

--- create_db.py
+++ create_db.py
@@ -193,10 +193,10 @@ class SecondGaesinBasicCulture:
 # sgbc = SecondGaesinBasicCulture()
 # sgbc.get_info()
 
-# file = pd.read_csv("./교육과정/1학기_일반교양.csv", encoding='utf-8' ,dtype=str)
-# file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
-# pd_file = pd.DataFrame(file).dropna()
-# # pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
+file = pd.read_csv("./교육과정/1학기_일반교양.csv", encoding='utf-8' ,dtype=str)
+file = file.filter(['학년','과목구분', '과목코드', '과목명', '학점', '학점부여' ])
+pd_file = pd.DataFrame(file).dropna()
+# pd_file = pd_file.query("개설학과=='컴퓨터공학과' or 개설학과=='소프트웨어학과' or 개설학과=='소프트웨어학부'" )
 
 # # 1학기 일반교양
 # class FirstBasicCulture:
