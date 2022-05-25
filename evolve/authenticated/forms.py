a553b31e7a8b83d31ec6223f1d21cfacd2da9218
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-25 11:48:10 +0900

    Error: 한글 인코딩 오류로 인해 한글 깨짐.. 해결중

--- evolve/authenticated/forms.py
+++ evolve/authenticated/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class UserForm(UserCreationForm):
-    email = forms.EmailField(label="¿Ã∏ﬁ¿œ")
+    email = forms.EmailField(label="Ïù¥Î©îÏùº")
 
     class Meta:
         model = User
