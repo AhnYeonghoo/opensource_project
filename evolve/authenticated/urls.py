494b75b998446c08f8b01b148c67161f8d3f8cb3
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-25 11:32:49 +0900

    feat: signup을 위한 form 제작 및 DB 제작

--- evolve/authenticated/urls.py
+++ evolve/authenticated/urls.py
@@ -7,4 +7,5 @@ app_name = "authenticated"
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('signup/', views.signup, name='signup'),
 ]
