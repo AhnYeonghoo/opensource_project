69c3bd63f3bcfce429dc10db2de0ae9e2c316f09
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-25 11:09:20 +0900

    feat: authenticated app의 urls를 main app에 include

--- evolve/evolve/urls.py
+++ evolve/evolve/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('search/', include("search.urls")),
+    path('authenticated/', include("authenticated.urls")),
     path('', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
