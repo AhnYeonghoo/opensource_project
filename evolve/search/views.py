deded203611b718073c9bfdf878756510719a6db
BiteSnail <rkwoal216@gmail.com>
2022-05-25 12:13:37 +0900

    modify : prnit_ge

--- evolve/search/views.py
+++ evolve/search/views.py
@@ -100,26 +100,28 @@ def contact(request):
     return render(request, "contact.html")
 
 def calculator(request):
-    c = load_data.MyInfo()
-    l = []
-    r = []
-    for field, my_score in c.my_ge.field.items():
-        a = {"field":field, "my_score":my_score, "min_score":c.min_ge.field[field], "sub_field":[]}
-        if c.is_specific(field):
-            for sub_field, m_score in c.my_ge.sub_field[field].items():
-                b = {"sub_field":sub_field,"my_score":m_score, "min_score":c.min_ge.sub_field[field][sub_field], "lec_info":[]}
+    my_info = load_data.MyInfo()
+    element = []
+    major = []
+    selection = []
+    for field, my_score in my_info.my_ge.field.items():
+        a = {"field":field, "my_score":my_score, "min_score":my_info.min_ge.field[field], "sub_field":[]}
+        if my_info.is_specific(field):
+            for sub_field, m_score in my_info.my_ge.sub_field[field].items():
+                b = {"sub_field":sub_field,"my_score":m_score, "min_score":my_info.min_ge.sub_field[field][sub_field], "lec_info":[]}
                 if m_score < b["min_score"]:
-                    b["lec_info"]=c.print_ge(sub_field)
+                    b["lec_info"]=my_info.print_ge(sub_field)
                 a["sub_field"].append(b)
         elif field == "전공필수":
-            r=c.print_need_lec()
+            major=my_info.print_need_lec()
         elif field == "전공선택":
-            c.print_major_selection()
-        l.append(a)
+            selection = my_info.print_ge("전공선택")
+        element.append(a)
     
     context = {
-        "sd":l,
-        "major":r,
+        "sd":element,
+        "major":major,
+        "selection":selection,
     }
     #c.print_my_lec()
     return render(request, "calculator.html", context)
