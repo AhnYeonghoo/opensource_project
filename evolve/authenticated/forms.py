457277acc470e2c8a430c99586e8277e46ec2787
AhnYeongHoo <dksdudgn00@naver.com>
2022-05-25 14:11:55 +0900

    fix: 인코딩 에러 문제 해결 및 회원가입 페이지 오류 fix

--- evolve/authenticated/forms.py
+++ evolve/authenticated/forms.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 
 
 class UserForm(UserCreationForm):
-    email = forms.EmailField(label="emails")
-
+    
+    email = forms.EmailField()
+    
     class Meta:
+        
         model = User
         fields = ("username", "password1", "password2", "email")
--- evolve/authenticated/views.py
+++ evolve/authenticated/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from authenticated.forms import UserForm
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def signup(request):
-    
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  
-            return redirect('index')
-    else:
-        form = UserForm()
-        
-    return render(request, "authenticated/signup.html", {'form':form})
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                                            username=request.POST['username'],
+                                            password=request.POST['password1'],
+                                            email=request.POST['email'],)
+            auth.login(request, user)
+            return redirect('/')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
 
-
+ 
